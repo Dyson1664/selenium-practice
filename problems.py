be0f6ec11c853abae367bfd1e5fbb7bfa6a9f487
david reilly <davidreilly02@gmail.com>
2024-06-01 11:42:48 +0700

    updatec dicts

--- problems.py
+++ problems.py
@@ -227,3 +227,14 @@ values = [10, 20, 30]
 dict = dict(zip(keys, values))
 print(dict)
 
+from word_search_generator import WordSearch
+
+puzzle = WordSearch("dog, cat, pig, horse, donkey, turtle, goat, sheep")
+puzzle.show()
+
+
+dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+
+dict3 = {**dict1, **dict2}
+print(dict3)
