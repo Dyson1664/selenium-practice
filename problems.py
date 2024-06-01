70c68767552899db90c75e39c417b2eca1ae1431
david reilly <davidreilly02@gmail.com>
2024-06-01 19:54:59 +0700

    added more answers

--- problems.py
+++ problems.py
@@ -238,3 +238,27 @@ dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
 
 dict3 = {**dict1, **dict2}
 print(dict3)
+
+sampleDict = {
+    "class": {
+        "student": {
+            "name": "Mike",
+            "marks": {
+                "physics": 70,
+                "history": 80
+            }
+        }
+    }
+}
+
+print(sampleDict['class']['student']['marks'].get('history'))
+
+
+
+
+employees = ['Kelly', 'Emma']
+defaults = {"designation": 'Developer', "salary": 8000}
+
+dict = dict.fromkeys(employees, defaults)
+
+print(dict)
